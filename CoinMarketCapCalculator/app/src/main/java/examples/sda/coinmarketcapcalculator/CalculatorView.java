0ffa7300cd130f29cc4b7741c6cdd39f14f4bf32
angie <angwittb@gmail.com>
2018-12-20 21:57:34 +0100

    Added files

--- CoinMarketCapCalculator/app/src/main/java/examples/sda/coinmarketcapcalculator/CalculatorView.java
+++ CoinMarketCapCalculator/app/src/main/java/examples/sda/coinmarketcapcalculator/CalculatorView.java
@@ -14,31 +14,6 @@ import examples.sda.coinmarketcapcalculator.model.CoinModel;
  * Created by angelika on 08.06.17.
  */
 
-public class CoinAdapter extends RecyclerView.Adapter<CoinViewHolder> {
-		private List<CoinModel> currencies = Collections.emptyList();
+public class CoinAdapter extends View {
 
-		@Override
-		public CoinViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-				View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_coin, parent, false);
-				return new CoinViewHolder(view);
-		}
-
-		@Override
-		public void onBindViewHolder(CoinViewHolder holder, int position) {
-				CoinModel coinModel = currencies.get(position);
-				holder.setNameText(coinModel.getName());
-				holder.setSymbolText(coinModel.getSymbol());
-				holder.setPriceUsdText(String.valueOf(coinModel.getPriceUsd()));
-				holder.setPriceBtcText(String.valueOf(coinModel.getPriceBtc()));
-		}
-
-		@Override
-		public int getItemCount() {
-				return currencies.size();
-		}
-
-		public void setData(List<CoinModel> data) {
-				this.currencies = data;
-				notifyDataSetChanged();
-		}
 }
